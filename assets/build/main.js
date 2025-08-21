d3a7673d9a68e9fb3c20bd8501be827a9be50be0
helle253 <nathanhellbhoy@gmail.com>
2025-08-21 08:44:36 -0500

    plug in some random quotes

--- assets/build/main.js
+++ assets/build/main.js
@@ -1,8 +1,9 @@
-function o(){let e=document.getElementById("app");if(!e)return;let n=new Date;e.innerHTML=`
+var e=["Just because you're paranoid doesn't mean they aren't after you.","The enemy is anybody who's going to get you killed, no matter which side he is on.","[They] agreed that it was neither possible nor necessary to educate people who never questioned anything.","Insanity is contagious.","What is a country? A country is a piece of land surrounded on all sides by boundaries, usually unnatural. Englishmen are dying for England, Americans are dying for America, Germans are dying for Germany, Russians are dying for Russia. There are now fifty or sixty countries fighting in this war. Surely so many countries can't all be worth dying for."];function o(){let n=document.getElementById("app");n&&(n.innerHTML=`
     <strong>js is alive.</strong>
     <div style="margin-top: .5rem; font-size: 0.9rem; color: #555;">
-      time: ${n.toLocaleString()}<br/>
-      mode: esm bundle via esbuild
+      <p>
+        ${e[Math.floor(Math.random()*e.length)]}
+      </p>
     </div>
-  `}o();
+  `)}o();
 //# sourceMappingURL=main.js.map
---
+++ src/quotes.ts
@@ -0,0 +1,7 @@
+export const quotes = [
+  "Just because you're paranoid doesn't mean they aren't after you.",
+  "The enemy is anybody who's going to get you killed, no matter which side he is on.",
+  '[They] agreed that it was neither possible nor necessary to educate people who never questioned anything.',
+  'Insanity is contagious.',
+  "What is a country? A country is a piece of land surrounded on all sides by boundaries, usually unnatural. Englishmen are dying for England, Americans are dying for America, Germans are dying for Germany, Russians are dying for Russia. There are now fifty or sixty countries fighting in this war. Surely so many countries can't all be worth dying for.",
+];
