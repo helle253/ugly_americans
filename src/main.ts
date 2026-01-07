dce45e8511d021bd793102fae6bdfbd0532d28f6
helle253 <nathanhellbhoy@gmail.com>
2026-01-07 12:47:00 -0600

    add console.log

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { quotes } from './quotes';
 
 function renderWelcome(): void {
   const js_container = document.getElementById('js-container');
-  if (!js_container) return;
+  if (!js_container) {
+    console.log('no js-container element found');
+    return;
+  }
+
   js_container.innerHTML = `
     <p class="mt-2 text-sm text-gray-600">
       ${quotes[Math.floor(Math.random() * quotes.length)]}
